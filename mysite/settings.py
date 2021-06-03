53b85f425d1ef5b37d0ce70891926eb273ef4cba
Lekhman-vold <volodya.l@yahoo.com>
2021-06-03 23:16:28 +0300

    add postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rampy',
+        'USER': 'naruto',
+        'PASSWORD': 'voloda2000',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
